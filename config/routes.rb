261d8a1070052cc18206f1e07e34bab8e2ae81b2
LARS BERG <larsgbr@gmail.com>
2014-06-17 09:37:34 -0500

    Added custom routes for authorization and allstatuses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 RandomMeet::Application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
   resources :statuses
+  get 'allstatuses', to: 'statuses#index', as: :allstatuses
   root to: 'statuses#index'
 
 
