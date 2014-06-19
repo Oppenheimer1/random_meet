46aa97bd32c276da656cbef39121cdbf718bf598
LARS BERG <larsgbr@gmail.com>
2014-06-19 13:54:46 -0500

    Added Gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me,	
-  				        :first_name, :last_name, :profile_name
-  # attr_accessible :title, :body
-
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :profile_name
+  
   validates :first_name, presence: true
 
   validates :last_name, presence: true
@@ -23,6 +23,14 @@ class User < ActiveRecord::Base
   has_many :statuses
 
   def full_name
-  	first_name + " " + last_name
+    first_name + " " + last_name
+  end
+
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+
+    "http://gravatar.com/avatar/#{hash}"
   end
 end
