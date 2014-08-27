5c451b751f2a86b0e7dc875baa73b4d428097a48
LARS BERG <larsgbr@gmail.com>
2014-08-27 14:40:39 -0500

    This should work

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
+  def new
+  	@s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: 201, acl: :public_read)
+  	@user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to :back, :alert => "Access denied."
+    end
+  end
+
+end
